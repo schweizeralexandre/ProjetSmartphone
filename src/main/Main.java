54d259b6f3047d7ba7087990001959295b0728e7
Alexandre <schweizer.alexandre@gmail.com>
2018-04-30 10:57:14 +0200

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Main.java
+ * Auteur: Alexandre Schweizer
+ * Date de création: 30 avr. 2018
+ */
+
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
