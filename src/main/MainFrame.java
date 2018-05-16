042b869d34d1b916ddd50400c1099e446c4cf704
ashan <ashan@DESKTOP-EF1R0VF>
2018-05-16 12:30:38 +0200

    changement des bordures pour le mainFrame

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -3,7 +3,15 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.RoundRectangle2D;
 import javax.swing.*;
+
+
+
+
+
 
 
 
@@ -31,6 +39,21 @@ public class MainFrame extends JFrame {
 	
 	add(mainScreen, BorderLayout.CENTER);
 	this.setUndecorated(true); // mets le JFrame sans bordures
+	
+	
+	this.addComponentListener(new ComponentAdapter() {
+        @Override
+         public void componentResized(ComponentEvent e) {
+             setShape(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 55, 55));
+         }
+     });
+	
+	
+	
+	
+	
+	
+	//this.setShape(new RoundRectangle2D.Double(0,0,getWidth(),getHeight(),20,40));
 	this.setResizable(false);
 	this.setSize(270, 540);
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
